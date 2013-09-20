5a85e79a9ae728c033e58cd4ac02f065e89a8f2c
Mariela Arboleda <luz.arboleda25@gmail.com>
2013-09-20 11:09:41 -0430

    bitacora

--- recursos/funciones.php
+++ recursos/funciones.php
@@ -186,7 +186,8 @@ function llenarLog($accion,$descripcion){
 			$accion="VACIO DE BITACORA";
 			break;	
 		}
-mysql_query("INSERT INTO bitacora values('','".$accion."',current_date,current_time,".$_SESSION["id_usuario"].",'".$descripcion."')",$conex) or die("Error Bitacora");
+		echo "INSERT INTO bitacora values(default,'".$accion."',current_date,current_time,CURTIME(),".$_SESSION["id_usuario"].",'".$descripcion."')"; 
+mysql_query("INSERT INTO bitacora values(default,'".$accion."',current_date,current_time,".$_SESSION["id_usuario"].",'".$descripcion."')",$conex) or die("Error Bitacora");
 
 }
 //Traer Menu principal
